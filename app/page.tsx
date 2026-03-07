69ad35a3c100751b18501be1f71acc1c4403814f
kaiox21 <kaioxavier50@gmail.com>
2026-03-07 17:10:02 -0300

    fix horarios

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ const TIME_SLOTS = [
   '08:00','08:30','09:00','09:30','10:00','10:30',
   '11:00','11:30','13:00','13:30','14:00','14:30',
   '15:00','15:30','16:00','16:30','17:00','17:30',
+  '18:00','18:30','19:00','19:30','20:00',
 ];
 const MONTHS_PT = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'];
 const WEEKDAYS  = ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb'];
